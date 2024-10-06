c78ab63f616cecfffe2caeba2d942b714a9cf94d
Retro21 <56453764+jekabso21@users.noreply.github.com>
2024-10-06 16:53:53 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://backend.victoria-pasparvalde.com/api';
-const API_URL = 'http://localhost:3003/api';
+const API_URL = 'https://backend.victoria-pasparvalde.com/api';
+// const API_URL = 'http://localhost:3003/api';
 
 const api = axios.create({
   baseURL: API_URL,
